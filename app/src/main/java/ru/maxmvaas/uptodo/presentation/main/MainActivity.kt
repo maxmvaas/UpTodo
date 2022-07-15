200a1cbaaed9c8361b68763d070d6926cb301f82
vasilev.maksim <maxmvaas@gmail.com>
2022-07-15 11:42:22 +0400

    add splash screen

---
+++ app/src/main/java/ru/maxmvaas/uptodo/presentation/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.maxmvaas.uptodo.presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import ru.maxmvaas.uptodo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
